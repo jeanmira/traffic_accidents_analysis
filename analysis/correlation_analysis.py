f11a11b72c4eedf8e4ea1bd9b04de8219f838070
mira <jeandemira@gmail.com>
2024-07-30 16:20:04 -0300

    Fix: Separate legends and improve layout in correlation analysis plot

--- analysis/correlation_analysis.py
+++ analysis/correlation_analysis.py
@@ -2,7 +2,7 @@
 # @Author: Jean Mira
 # @Date:   2024-07-30 15:20:35
 # @Last Modified by:   Jean Mira
-# @Last Modified time: 2024-07-30 15:28:27
+# @Last Modified time: 2024-07-30 16:17:32
 
 
 import pandas as pd
@@ -16,7 +16,7 @@ def correlation_analysis(cleaned_traffic_data_path, cleaned_accident_data_path):
     traffic_data['mes_ano'] = pd.to_datetime(
         traffic_data['mes_ano'], format='%d/%m/%Y')
     monthly_traffic_volume = traffic_data.groupby(
-        traffic_data['mes_ano'].dt.to_period('M')).sum()['volume_total']
+        traffic_data['mes_ano'].dt.to_period('M'))['volume_total'].sum()
 
     accident_data['data_inversa'] = pd.to_datetime(
         accident_data['data_inversa'])
@@ -30,21 +30,27 @@ def correlation_analysis(cleaned_traffic_data_path, cleaned_accident_data_path):
 
     correlation = traffic_accidents_df.corr()
 
-    plt.figure(figsize=(12, 6))
-    traffic_accidents_df['Volume_Trafego'].plot(
-        kind='line', marker='o', color='blue', label='Volume de Tráfego')
+    fig, ax1 = plt.subplots(figsize=(12, 6))
+
+    color = 'tab:blue'
+    ax1.set_xlabel('Mês')
+    ax1.set_ylabel('Volume de Tráfego', color=color)
+    ax1.plot(traffic_accidents_df.index.astype(
+        str), traffic_accidents_df['Volume_Trafego'], color=color, marker='o', label='Volume de Tráfego')
+    ax1.tick_params(axis='y', labelcolor=color)
+    ax1.legend(loc='upper left')
+
+    ax2 = ax1.twinx()
+    color = 'tab:red'
+    ax2.set_ylabel('Número de Acidentes', color=color)
+    ax2.plot(traffic_accidents_df.index.astype(
+        str), traffic_accidents_df['Numero_Acidentes'], color=color, marker='o', label='Número de Acidentes')
+    ax2.tick_params(axis='y', labelcolor=color)
+    ax2.legend(loc='upper right')
+
     plt.title('Volume de Tráfego e Número de Acidentes em MG (2020)')
-    plt.ylabel('Volume de Tráfego')
     plt.grid(True)
-    plt.legend()
-
-    plt.twinx()
-    traffic_accidents_df['Numero_Acidentes'].plot(
-        kind='line', marker='o', color='red', label='Número de Acidentes')
-    plt.ylabel('Número de Acidentes')
-    plt.grid(True)
-    plt.legend()
-
+    fig.tight_layout()
     plt.show()
 
     print('Correlação:', correlation)
--- analysis/exploratory_analysis.py
+++ analysis/exploratory_analysis.py
@@ -2,7 +2,7 @@
 # @Author: Jean Mira
 # @Date:   2024-07-30 15:20:35
 # @Last Modified by:   Jean Mira
-# @Last Modified time: 2024-07-30 15:28:15
+# @Last Modified time: 2024-07-30 16:15:56
 
 
 import pandas as pd
@@ -16,13 +16,15 @@ def exploratory_analysis(cleaned_traffic_data_path, cleaned_accident_data_path):
     traffic_data['mes_ano'] = pd.to_datetime(
         traffic_data['mes_ano'], format='%d/%m/%Y')
     monthly_traffic_volume = traffic_data.groupby(
-        traffic_data['mes_ano'].dt.to_period('M')).sum()['volume_total']
+        traffic_data['mes_ano'].dt.to_period('M'))['volume_total'].sum()
 
     plt.figure(figsize=(12, 6))
-    monthly_traffic_volume.plot(kind='line', marker='o')
+    monthly_traffic_volume.plot(
+        kind='line', marker='o', label='Volume de Tráfego')
     plt.title('Volume de Tráfego nas Praças de Pedágio em MG (2020)')
     plt.xlabel('Mês')
     plt.ylabel('Volume Total de Veículos')
+    plt.legend(loc='best')
     plt.grid(True)
     plt.show()
 
@@ -32,20 +34,24 @@ def exploratory_analysis(cleaned_traffic_data_path, cleaned_accident_data_path):
         accident_data['data_inversa'].dt.to_period('M')).size()
 
     plt.figure(figsize=(12, 6))
-    monthly_accidents.plot(kind='bar')
+    monthly_accidents.plot(kind='bar', color='skyblue',
+                           edgecolor='black', label='Número de Acidentes')
     plt.title('Número de Acidentes em MG (2020)')
     plt.xlabel('Mês')
     plt.ylabel('Número de Acidentes')
+    plt.legend(loc='best')
     plt.grid(True)
     plt.show()
 
     weekday_accidents = accident_data['dia_semana'].value_counts().sort_index()
 
     plt.figure(figsize=(12, 6))
-    weekday_accidents.plot(kind='bar')
+    weekday_accidents.plot(kind='bar', color='lightgreen',
+                           edgecolor='black', label='Número de Acidentes')
     plt.title('Número de Acidentes por Dia da Semana em MG (2020)')
     plt.xlabel('Dia da Semana')
     plt.ylabel('Número de Acidentes')
+    plt.legend(loc='best')
     plt.xticks(rotation=45)
     plt.grid(True)
     plt.show()
@@ -53,10 +59,12 @@ def exploratory_analysis(cleaned_traffic_data_path, cleaned_accident_data_path):
     causes_accidents = accident_data['causa_acidente'].value_counts().head(10)
 
     plt.figure(figsize=(12, 6))
-    causes_accidents.plot(kind='bar')
+    causes_accidents.plot(kind='bar', color='salmon',
+                          edgecolor='black', label='Número de Acidentes')
     plt.title('Principais Causas de Acidentes em MG (2020)')
     plt.xlabel('Causa do Acidente')
     plt.ylabel('Número de Acidentes')
+    plt.legend(loc='best')
     plt.xticks(rotation=45)
     plt.grid(True)
     plt.show()
@@ -65,10 +73,12 @@ def exploratory_analysis(cleaned_traffic_data_path, cleaned_accident_data_path):
         'feridos_leves', 'feridos_graves', 'ilesos', 'mortos']].sum()
 
     plt.figure(figsize=(12, 6))
-    severity_accidents.plot(kind='bar')
+    severity_accidents.plot(kind='bar', color='lightcoral',
+                            edgecolor='black', label='Número de Pessoas')
     plt.title('Severidade dos Acidentes em MG (2020)')
     plt.xlabel('Tipo de Severidade')
     plt.ylabel('Número de Pessoas')
+    plt.legend(loc='best')
     plt.xticks(rotation=0)
     plt.grid(True)
     plt.show()
